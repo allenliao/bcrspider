014af6e8f6d4555b6e32c86535f2434c3a331734
Unknown <allenliao@gmail.com>
2017-11-06 00:59:39 +0800

    login and open table

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
+﻿import json
+from sikuli.Sikuli import *
 
 def readConfig():
     buConfigFile = open('buconfig.json','r')
@@ -7,10 +8,45 @@ def readConfig():
     #print buConfigStr
     
     buConfigObj=json.loads(buConfigStr)
-    print buConfigObj['buurl']
+    buurl=buConfigObj['buurl']
+    username=buConfigObj['username']
+    password=buConfigObj['password']
+    print buurl
+    openWebSiter(buurl)
+    loginAndOpenGame(username,password)
+
+def openWebSiter(buurl):
+    openApp(r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe "+buurl)
+    
+def loginAndOpenGame(username,password):
+    if s.find("account_cache.png"):
+        s.find("loginBtn.png").click()
+    else:
+        s.find("account.png").click()
+        type(username)
+        s.find("password.png").click()
+        type(password)
+        s.find("loginBtn.png").click()
+    
+    s.find("loginConfirmBtn.png").click()
+    s.find("LDRBtn.png").click()#真人娛樂
+    s.find("SAGameBtn.png").click()#沙龍魚樂
+    wait("multiGameBtn.png", FOREVER)
+    s.find("multiGameBtn.png").click()#多人頭注
+    wait("BCR01Btn.png", 5)
+    s.find("BCR01Btn.png").click()#Table1
+    s.find("selectTable.png").click()#select Table
+    s.find("BCR02Btn.png").click()#Table2
+    s.find("selectTable.png").click()#select Table
+    s.find("BCR03Btn.png").click()#Table3
+    s.find("closeHint.png").click()#close hint
+
+    
+
     
 
 if __name__ == "__main__":
+    s = Screen()
     readConfig()
     #load config
     #login
